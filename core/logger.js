235e35bd3ad3567931613c08e6b87def486ec997
buzzxu <xiang.leau@gmail.com>
2018-11-22 15:07:13 +0800

    增加log4js

--- core/logger.js
+++ core/logger.js
@@ -2,9 +2,9 @@ var log4js = require('log4js');
 
 log4js.configure({
     appenders: {
-        access: { type: 'dateFile', filename: '/data/logs/images/access.log', pattern: "_yyyy-MM-dd",maxLogSize:10485760,backups: 3,compress:true},
-        image: { type: 'dateFile', filename: '/data/logs/images/image.log', pattern: "_yyyy-MM-dd",maxLogSize:10485760,backups: 3,compress:true},
-        error: { type: 'dateFile', filename: '/data/logs/images/error.log', pattern: "_yyyy-MM-dd",maxLogSize:10485760,backups: 3,compress:true},
+        access: { type: 'dateFile', filename: '/data/logs/access.log', pattern: ".yyyy-MM-dd",maxLogSize:10485760,backups: 3,compress:true},
+        image: { type: 'dateFile', filename: '/data/logs/image.log', pattern: ".yyyy-MM-dd",maxLogSize:10485760,backups: 3,compress:true},
+        error: { type: 'dateFile', filename: '/data/logs/error.log', pattern: ".yyyy-MM-dd",maxLogSize:10485760,backups: 3,compress:true},
         console: { type: "console"}
     },
     replaceConsole: true,
@@ -20,8 +20,5 @@ module.exports ={
         access: log4js.getLogger('access'),
         image: log4js.getLogger('image'),
         error: log4js.getLogger('error'),
-        express: log4js.connectLogger(log4js.getLogger('access'), {level: 'info'}),
-        isDebug: function(category) {
-            return (log4js.levels.DEBUG.level >= category.level.level);
-        }
+        express: log4js.connectLogger(log4js.getLogger('access'), {level: 'info'})
 };
